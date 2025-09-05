84f9535a06dd786a76b3334280dc06d5ad0c39bc
vicsystems <victorasuquob@gmail.com>
2025-09-05 17:34:02 +0100

    site update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,5 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+
+          Schema::defaultStringLength(191);
     }
 }
